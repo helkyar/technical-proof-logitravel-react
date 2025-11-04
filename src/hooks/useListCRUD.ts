7615451be98bb1abde981fa271b110ff1cf8ac6c
JaviMediaset <jpalaciosb@xt.mediaset.es>
2025-11-04 12:40:54 +0100

    feat(app): add persistence

--- src/hooks/useListCRUD.ts
+++ src/hooks/useListCRUD.ts
@@ -2,11 +2,19 @@ import { useState } from "react";
 // utils
 import { v4 as uuidv4 } from "uuid";
 //data
-import initialItems from "data/initialItems.json";
+import initialItems from "../data/initialItems.json";
 
 export function useListCRUD() {
-  const [prevState, setPrevState] = useState(initialItems);
+  const savedItems = () => {
+    const sotoredItems = localStorage.getItem("storedListItems");
+    return sotoredItems ? JSON.parse(sotoredItems) : initialItems;
+  };
+  const [prevState, setPrevState] = useState(savedItems());
   const [items, setItems] = useState(initialItems);
+
+  const saveItems = (itemsToSave: typeof initialItems) => {
+    localStorage.setItem("storedListItems", JSON.stringify(itemsToSave));
+  };
 
   const addItem = (content: string) => {
     setPrevState(items);
@@ -15,25 +23,44 @@ export function useListCRUD() {
       content,
       selected: false,
     };
-    setItems((prevItems) => [...prevItems, newItem]);
+    setItems((prevItems) => {
+      const newItems = [...prevItems, newItem];
+      saveItems(newItems);
+      return newItems;
+    });
   };
+
   const toggleSelection = (id: string) => {
-    setItems((prevItems) =>
-      prevItems.map((item) =>
+    setItems((prevItems) => {
+      const newItems = prevItems.map((item) =>
         item.id === id ? { ...item, selected: !item.selected } : item
-      )
-    );
+      );
+      saveItems(newItems);
+      return newItems;
+    });
   };
+
   const deleteItem = (id: string) => {
     setPrevState(items);
-    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+    setItems((prevItems) => {
+      const newItems = prevItems.filter((item) => item.id !== id);
+      saveItems(newItems);
+      return newItems;
+    });
   };
+
   const deleteSelectedItems = () => {
     setPrevState(items);
-    setItems((prevItems) => prevItems.filter((item) => !item.selected));
+    setItems((prevItems) => {
+      const newItems = prevItems.filter((item) => !item.selected);
+      saveItems(newItems);
+      return newItems;
+    });
   };
+
   const undoLastChange = () => {
     setItems(prevState);
+    saveItems(prevState);
   };
 
   return {
